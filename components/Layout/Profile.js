a8462f01a8a0537636ba828023a5785528b35ef7
Hamid Ziadzadeh <hamidziadzadeh@gmail.com>
2020-10-24 15:25:12 +0330

    feat(layout) :: add header to profile layout

--- components/Layout/Profile.js
+++ components/Layout/Profile.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Divider } from 'antd';
 
 import Page from '../Page';
 import ProfileNavigation from "../ProfileNavigation";
 
-const ProfileLayout = ({title, breadcrumb, ...props}) => {
+const ProfileLayout = ({title, breadcrumb, actions, ...props}) => {
     return (
-        <Page title={title} breadcrumb={breadcrumb}>
+        <Page title={false} breadcrumb={breadcrumb}>
             <Row gutter={32} className="flex md:flex-row flex-col">
                 <Col xs={24} md={8} lg={6} className={'hidden md:flex md:flex-col'}>
                     <ProfileNavigation/>
                 </Col>
                 <Col xs={24} md={16} lg={18}>
-                    {props.children}
+                    {title && (
+                        <div className="flex flex-col">
+                            <div className="flex items-center justify-between">
+                                <h1 className="page__title text-2xl text-label font-medium my-0">{title}</h1>
+                                <div className="actions flex items-center">{actions || null}</div>
+                            </div>
+                            <Divider/>
+                        </div>
+                    )}
+                    <div className="w-full">
+                        {props.children}
+                    </div>
                 </Col>
             </Row>
         </Page>
