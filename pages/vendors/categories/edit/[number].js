898fdfc9a5285e606665d15b29bcb8145e48f1d4
Hamid Ziadzadeh <hamidziadzadeh@gmail.com>
2020-10-25 21:52:29 +0330

    design(vendor categories) :: create vendor categories pages

---
+++ pages/vendors/categories/edit/[number].js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import { Form, Row, Col, Input, Select, Upload, Radio, Button } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+
+import Page from "../../../../components/Page";
+import routes from "../../../../constants/routes";
+import {useRouter} from "next/router";
+
+const { Item } = Form;
+const { Option } = Select;
+
+const EditCategory = props => {
+    const router = useRouter();
+    const [name, setName] = useState('');
+    const [form] = Form.useForm();
+
+
+    useEffect(() => {
+        if(router) {
+            setName(router.query.number)
+        }
+    }, [router])
+    const breadcrumb = [
+        {
+            title: 'Store',
+            href: routes.vendors.index
+        },
+        {
+            title: `Edit Category`,
+        },
+        {
+            title: name
+        }
+    ]
+    return (
+        <Page title={`Edit Category ${name}`} breadcrumb={breadcrumb}>
+            <Form form={form} layout={'vertical'}>
+                <Row gutter={24}>
+                    <Col xs={24} md={12} lg={8}>
+                        <Item name={'name'} label={'Name'}>
+                            <Input placeholder={'Name'}/>
+                        </Item>
+                    </Col>
+                    <Col xs={24} md={12} lg={8}>
+                        <Item name={'parentCategory'} label={'Parent Category'}>
+                            <Select placeholder={'Select Weight Unit'}>
+                                {[...Array(12)].map((item, index) => {
+                                    return (
+                                        <Option value={`C${index + 1}`} key={`cat-${index + 1}`}>C#{index + 1}</Option>
+                                    )
+                                })}
+                            </Select>
+                        </Item>
+                    </Col>
+                    <Col xs={24} className={'flex flex-row-reverse md:mt-16 mt-6'}>
+                        <Item>
+                            <Button type="primary" block className={'w-32 ml-5'}>
+                                Save
+                            </Button>
+                        </Item>
+                        <Item>
+                            <Button danger className={'w-32'}>Cancel</Button>
+                        </Item>
+                    </Col>
+                </Row>
+            </Form>
+        </Page>
+    )
+}
+
+export default EditCategory;
