244b3e8936b2a4f715d3ddc3a41b3ed001484198
Hamid Ziadzadeh <hamidziadzadeh@gmail.com>
2020-10-25 21:55:05 +0330

    update(avatar) :: make content optional

--- components/UI/Avatar/index.js
+++ components/UI/Avatar/index.js
@@ -5,11 +5,11 @@ import { StarFilled, StarOutlined } from '@ant-design/icons';
 const Avatar = props => {
     return (
         <div className="flex items-center">
-
             <img src="/images/profile.png" alt="profile" className="rounded-full cursor-pointer" />
-            <div className="flex flex-col justify-center ml-3">
-                <span className="text-paragraph text-xs">{props.title}</span>
-                <span>
+            {!props.justImage && (
+                <div className="flex flex-col justify-center ml-3">
+                    <span className="text-paragraph text-xs">{props.title}</span>
+                    <span>
                     <Space size={4} className="flex items-center text-yellow-400">
                         <StarFilled className={'text-xs'}/>
                         <StarFilled className={'text-xs'}/>
@@ -18,7 +18,8 @@ const Avatar = props => {
                         <StarOutlined className={'text-xs'}/>
                     </Space>
                 </span>
-            </div>
+                </div>
+            )}
         </div>
     )
 }
