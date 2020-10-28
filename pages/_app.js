32e36af0f46acc086e0051a8205fa3fb533f2793
Hamid Ziadzadeh <hamidziadzadeh@gmail.com>
2020-10-28 23:58:29 +0330

    update(app) :: add checking authetication feature to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import App from 'next/app';
+
+import cookie from 'cookie';
 
 import '../styles/less/index.less';
 import '../styles/scss/global.scss';
 import AppProvider from "../providers/AppProvider";
 import Layout from "../components/Layout";
+import RouterChanger from "../components/RouteChanger";
 
-function MyApp({ Component, pageProps }) {
-    const forceLayout = pageProps.hasOwnProperty('forceLayout') ? pageProps.forceLayout : false;
-    return (
-        <AppProvider>
-            <Layout style={{ background: 'white' }} forceLayout={forceLayout}>
-                <Component {...pageProps} />
-            </Layout>
-        </AppProvider>
-    )
+class MyApp extends App {
+    render() {
+
+        const { Component, pageProps, authenticated } = this.props;
+        const forceLayout = pageProps.hasOwnProperty('forceLayout') ? pageProps.forceLayout : false;
+        return (
+            <AppProvider authenticated={authenticated}>
+                <RouterChanger/>
+                <Layout style={{ background: 'white' }} forceLayout={forceLayout}>
+                    <Component {...pageProps} />
+                </Layout>
+            </AppProvider>
+        )
+    }
 }
+
+MyApp.getInitialProps = async (appContext) => {
+    let authenticated = false;
+    const request = appContext.ctx.req;
+    if (request) {
+        request.cookies = cookie.parse(request.headers.cookie || '');
+        authenticated = !!request.cookies.token;
+    }
+
+    // Call the page's `getInitialProps` and fill `appProps.pageProps`
+    const appProps = await App.getInitialProps(appContext);
+
+    return { ...appProps, authenticated };
+};
 
 export default MyApp
