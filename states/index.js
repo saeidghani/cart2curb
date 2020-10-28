6adc3427023a905054f4cf6906eb7ca27ebd1f84
Hamid Ziadzadeh <hamidziadzadeh@gmail.com>
2020-10-29 00:02:27 +0330

    update(state) :: add store configuration

--- states/index.js
+++ states/index.js
@@ -1,1 +1,25 @@
-// @todo: Initialize store
+import { init } from '@rematch/core';
+import immerPlugin from "@rematch/immer";
+import persistPlugin from "@rematch/persist";
+import loadingPlugin from '@rematch/loading';
+import storage from 'redux-persist/lib/storage'
+
+import * as models from './models';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const store = init({
+    models,
+    plugins: [
+        immerPlugin(),
+        persistPlugin(persistConfig),
+        loadingPlugin()
+    ]
+})
+
+export const getStore = () => store;
+
+export default store;
