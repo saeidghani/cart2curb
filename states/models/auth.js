ccf3747dc0888e533f3aa68f9b349630b61a4076
Hamid Ziadzadeh <hamidziadzadeh@gmail.com>
2020-10-28 23:47:55 +0330

    states(api) :: add basic models for auth and profile & setup redux(rematch) store

---
+++ states/models/auth.js
@@ -0,0 +1,134 @@
+import {api} from '../../lib/api';
+import axios from 'axios';
+import {message} from "antd";
+import {emitter} from "../../helpers/emitter";
+import routes from "../../constants/routes";
+
+export const auth = {
+    state: {
+        isLoggedIn: false,
+        token: null,
+        resetToken: null,
+        isReseted: false,
+        resetAttempt: 0,
+    },
+    reducers: {
+        authenticate: (state, payload) => {
+            window.localStorage.setItem("jwt_token", payload.token);
+            state.isLoggedIn = true;
+            state.token = payload.token;
+        },
+        logoutSync: (state) => {
+            window.localStorage.setItem("jwt_token", "");
+
+            state.isLoggedIn = false;
+            state.token = null;
+        },
+        setResetToken: (state, payload) => {
+            state.resetToken = payload.token;
+            state.isReseted = true;
+        },
+        destroyResetToken: (state) => {
+            state.resetToken = null;
+            state.isReseted = false;
+            state.resetAttempt = 0;
+        },
+        increaseAttempt: (state) => {
+            state.resetAttempt++;
+        }
+    },
+    effects: dispatch => ({
+        async login(body) {
+            try {
+                const res = await api.post('auth/login', body);
+                if(res.data.success) {
+                    dispatch.auth.authenticate({
+                        token: res.data.data.token
+                    });
+                    message.success('You Logged in successfully!');
+                    return true;
+                } else {
+                    message.error('Username Or Password is wrong', 5);
+                }
+
+                return false;
+            } catch(e) {
+                message.error('Username Or Password is wrong', 5);
+                return false;
+            }
+        },
+        async logout() {
+            dispatch.auth.logoutSync();
+            await api.post('auth/logout', {})
+            return true;
+        },
+        async register(body) {
+            try {
+                const res = await api.post('auth/register', body);
+                const data = res.data;
+                if(data.success) {
+                    dispatch.auth.authenticate({
+                        token: data.data.token
+                    });
+                    message.success('Your Registration was Complete');
+                    emitter.emit('change-route', {
+                        path: routes.auth.register.accountInfo,
+                    })
+                    return true;
+                } else {
+                    message.error('Something went wrong', 5);
+                }
+                return false;
+            } catch(e) {
+                console.log(e);
+                message.error('Something went wrong', 5);
+
+                return false;
+            }
+
+        },
+        async forgetPassword(body) {
+            try {
+                const res = await api.put('auth/forgetPassword', body);
+                if(res.data.success) {
+                    message.success('Reset Link was sent to your email');
+                }
+                dispatch.auth.destroyResetToken()
+            } catch(e) {
+                console.log(e);
+                message.error('Something went wrong', 5);
+            }
+        },
+        async resetPassword(body) {
+            try {
+                const res = await api.put('auth/resetPassword', body);
+                if(res.data.success) {
+                    dispatch.auth.setResetToken({ token: body.token });
+                    message.success('Your Password was changed!');
+                    emitter.emit('change-route', {
+                        path: routes.auth.resetPassword.submitted,
+                    })
+                } else {
+                    message.error('Your Token is not valid or expired', 5);
+                }
+
+            } catch(e) {
+                message.error('Your Token is not valid or expired', 5);
+            }
+        },
+        async changePassword(body) {
+            try {
+                const res = await api.post('auth/changePassword', body);
+                if(res.data.success) {
+                    message.success('Password changed successfully')
+                    emitter.emit('change-route', {
+                        path: routes.profile.index,
+                    })
+                    return true;
+                }
+            } catch(e) {
+                message.error('Something went wrong', 5);
+            }
+        }
+    })
+}
---
+++ states/models/profile.js
@@ -0,0 +1,171 @@
+import  { api } from "../../lib/api";
+import {message} from "antd";
+import {emitter} from "../../helpers/emitter";
+
+
+export const profile = {
+    state: {
+        profile: {},
+        addresses: [],
+        payments: [],
+    },
+    reducers: {
+        setProfile: (state, payload) => {
+            state.profile = payload.profile;
+        },
+        setAddresses: (state, payload) => {
+            state.addresses = payload.addresses
+        },
+        setPayments: (state, payload) => {
+            state.payments = payload.payments
+        }
+    },
+    effects: dispatch => ({
+        async getProfile(config) {
+            try {
+                const res = await api.get('profile/getProfile', config);
+                dispatch.profile.setProfile({
+                    profile: res.data.data
+                })
+                if(res.data.success) {
+                    return res.data.data;
+                } else {
+                    return {};
+                }
+            } catch(e) {
+                console.log(e);
+            }
+        },
+        async updateProfile(body) {
+
+            try {
+                const res = await api.post('auth/registerInfo', body);
+                if(res.data.success) {
+                    const resData = res.data;
+                    dispatch.profile.setProfile({
+                        addresses: resData.data
+                    });
+                }
+                message.success('Your Profile Information was updated!');
+            } catch(e) {
+                const errorData = e.response.data;
+                if(errorData.hasOwnProperty('errors')) {
+                    errorData.errors.map(err => {
+                        message.error(err.message || 'Something Went wrong', 4)
+                    })
+                } else {
+                    message.error('Something went wrong', 5);
+                }
+            }
+
+        },
+        async getAddresses(config) {
+            try {
+                const res = await api.get('profile/address/getAddresses', config);
+                dispatch.profile.setAddresses({
+                    profile: res.data.data
+                })
+                if(res.data.success) {
+                    return res.data.data;
+                } else {
+                    return {};
+                }
+            } catch(e) {
+                console.log(e);
+            }
+        },
+        async deleteAddress(id) {
+            try {
+                const res = await api.post('profile/address/delete', { id });
+                if(res.data.success) {
+                    message.success('Address Deleted successfully!', 5);
+                    return true;
+                } else {
+                    message.error('Something went wrong');
+                    return false;
+                }
+            } catch(e) {
+                console.log(e);
+                message.error('Something went wrong');
+                return false;
+            }
+        },
+        async addAddress(body) {
+            try {
+                const res = await api.post('profile/address/add', body);
+                if(res.data.success) {
+                    const resData = res.data;
+                    dispatch.profile.setProfile({
+                        addresses: resData.data
+                    });
+                }
+                message.success('New Address Added!');
+                return true;
+            } catch(e) {
+                const errorData = e.response.data;
+                if(errorData.hasOwnProperty('errors')) {
+                    errorData.errors.map(err => {
+                        message.error(err.message || 'Something Went wrong', 4)
+                    })
+                } else {
+                    message.error('Something went wrong', 5);
+                }
+            }
+        },
+        async getPayments(config) {
+
+            try {
+                const res = await api.get('profile/payments/getPayments', config);
+                dispatch.profile.setPayments({
+                    profile: res.data.data
+                })
+                if(res.data.success) {
+                    return res.data.data;
+                } else {
+                    return {};
+                }
+            } catch(e) {
+                console.log(e);
+            }
+        },
+        async deletePayment(id) {
+
+            try {
+                const res = await api.post('profile/payments/delete', { id });
+                if(res.data.success) {
+                    message.success('Card Deleted successfully!', 5);
+                    return true;
+                } else {
+                    message.error('Something went wrong');
+                    return false;
+                }
+            } catch(e) {
+                console.log(e);
+                message.error('Something went wrong');
+                return false;
+            }
+        },
+        async addPayment(body) {
+            try {
+                const res = await api.post('profile/payments/add', body);
+                if(res.data.success) {
+                    const resData = res.data;
+                    dispatch.profile.setProfile({
+                        addresses: resData.data
+                    });
+                }
+                message.success('New Card Added!');
+                return true;
+            } catch(e) {
+                const errorData = e.response.data;
+                if(errorData.hasOwnProperty('errors')) {
+                    errorData.errors.map(err => {
+                        message.error(err.message || 'Something Went wrong', 4)
+                    })
+                } else {
+                    message.error('Something went wrong', 5);
+                }
+            }
+        }
+    })
+}
