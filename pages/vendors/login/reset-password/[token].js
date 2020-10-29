df90c2fada1e50d550ff09fed4d443807181179b
Hamid Ziadzadeh <hamidziadzadeh@gmail.com>
2020-10-29 18:17:06 +0330

    logics(vendor reset password) :: add logics for reset password

--- pages/vendors/login/reset-password/[token].js
+++ pages/vendors/login/reset-password/[token].js
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import Page from '../../../../components/Page';
-import {Button, Col, Form, Input, Row} from "antd";
+import {Button, Col, Form, Input, message, Row} from "antd";
+import routes from "../../../../constants/routes";
+import {useRouter} from "next/router";
+import {useDispatch, useSelector} from "react-redux";
+import {useRedirectAuthenticated} from "../../../../hooks/auth";
 
 const { Item } = Form;
 
 const ResetPassword = props => {
     const [form] = Form.useForm();
+    const router = useRouter();
+    const loading = useSelector(state => state.loading.effects.vendorAuth.resetPassword);
+    const dispatch = useDispatch()
+    const redirect = useRedirectAuthenticated();
+
+    useEffect(() => {
+        redirect();
+    }, [redirect])
+
+
 
     const breadcrumb = [
         {
             title: "Login",
-            href: '/login'
+            href: routes.vendors.auth.login
         },
         {
             title: 'Forgot Password',
-            href: '/login/forget-password',
+            href: routes.vendors.auth.forgetPassword,
         },
         {
             title: 'Reset Password',
         }
     ]
+
+    useEffect(() => {
+        let token = router.query.token || undefined;
+        if((typeof token === 'string' && token.length === 0)) {
+            message.error('Please use valid reset password link!', 5);
+            router.push(routes.vendors.auth.login);
+        }
+    }, [router])
+
+
+    const submitHandler = (values) => {
+        const { password } = values;
+        const body = {
+            newPassword: password,
+            token: router.query.token
+        }
+        dispatch.vendorAuth.resetPassword(body)
+    }
+
+    const checkValidation = (errorInfo) => {
+        message.error(errorInfo.errorFields[0].errors[0], 5);
+    }
+
 
     return (
         <Page title={'Reset Password'} breadcrumb={breadcrumb}>
@@ -30,15 +67,46 @@ const ResetPassword = props => {
                         form={form}
                         layout="vertical"
                         className="flex flex-col"
+                        onFinish={submitHandler}
+                        onFinishFailed={checkValidation}
                     >
-                        <Item name={'password'} label={'New Password'}>
-                            <Input type={'password'} placeholder='New Password' className={'mb-3'} />
+                        <Item name={'password'}
+                              label={'New Password'}
+                              hasFeedback
+                              rules={[
+                                  {
+                                      required: true,
+                                      message: 'Please input your password!',
+                                  },
+                                  {
+                                      min: 6,
+                                      message: "Password should be at least 6 character"
+                                  }
+                              ]}>
+                            <Input.Password placeholder="New Password"/>
                         </Item>
-                        <Item name={'password-confirm'} label={'New Password Confirm'}>
-                            <Input type={'password'} placeholder='New Password Confirm' className={'mb-3'} />
+                        <Item name={'password-confirm'}
+                              label={'New Password Confirm'}
+                              dependencies={['password']}
+                              hasFeedback
+                              rules={[
+                                  {
+                                      required: true,
+                                      message: 'Please confirm your password!',
+                                  },
+                                  ({getFieldValue}) => ({
+                                      validator(rule, value) {
+                                          if (!value || getFieldValue('password') === value) {
+                                              return Promise.resolve();
+                                          }
+                                          return Promise.reject('The two passwords that you entered do not match!');
+                                      },
+                                  }),
+                              ]}>
+                            <Input.Password placeholder="New Password Confirm"/>
                         </Item>
                         <Item>
-                            <Button type="primary" block>
+                            <Button type="primary" htmlType={'submit'} block loading={loading}>
                                 Submit
                             </Button>
                         </Item>
