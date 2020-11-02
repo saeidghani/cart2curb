d25aa1091e978a28a2826bf02d00ceb66ddd5785
Hamid Ziadzadeh <hamidziadzadeh@gmail.com>
2020-11-02 20:10:12 +0330

    fix(vendor login) :: fix vendor login page

--- pages/vendors/login/index.js
+++ pages/vendors/login/index.js
@@ -56,6 +56,10 @@ const Login = props => {
                                         required: true,
                                         message: "Please enter your Email Address"
                                     },
+                                    {
+                                        pattern: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
+                                        message: "Please enter valid Email Address"
+                                    }
                                 ]}>
                                     <Input placeholder="Email Address" />
                                 </Item>
@@ -67,6 +71,10 @@ const Login = props => {
                                     {
                                         required: true,
                                         message: "Password Field is required"
+                                    },
+                                    {
+                                        min: 6,
+                                        message: "Password should be 6 characters at least"
                                     }
                                 ]}>
                                     <Input type="password" placeholder="Password" />
