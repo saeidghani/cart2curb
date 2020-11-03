a7bc4f792fb987ebe7740a691173d2ef96ee4091
Hamid Ziadzadeh <hamidziadzadeh@gmail.com>
2020-11-03 15:38:23 +0330

    update(delivery) :: update delivery page design changes

--- pages/cart/delivery/index.js
+++ pages/cart/delivery/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     Button,
     Input,
@@ -13,12 +13,23 @@ import {
 } from 'antd';
 
 import Page from '../../../components/Page';
+import cookie from "cookie";
+import routes from "../../../constants/routes";
+import {getStore} from "../../../states";
+import GoogleMap from "../../../components/Map";
+import {useCities, useProvinces} from "../../../hooks/region";
+import Link from "next/link";
 
 const { Item } = Form;
 const { Option } = Select;
 
 const Delivery = props => {
     const [form] = Form.useForm();
+    const [newAddress, setNewAddress] = useState(false);
+    const [province, setProvince] = useState('');
+    const [marker, setMarker] = useState({ position: {}})
+    const provinces = useProvinces();
+    const cities = useCities(province);
 
     const breadcrumb = [
         {
@@ -29,6 +40,26 @@ const Delivery = props => {
             title: 'Delivery'
         }
     ]
+
+    const { addresses } = props;
+
+    const changeMarkerPosition = (e, map, position) => {
+        const newPosition = {
+            lat: position.latLng.lat(),
+            lng: position.latLng.lng()
+        }
+        setMarker({
+            position: newPosition
+        });
+    }
+
+    const changeAddressHandler = (e) => {
+        if(e.target.value === 'new') {
+            setNewAddress(true)
+        } else {
+            setNewAddress(false)
+        }
+    }
 
     return (
         <Page title={'Delivery Time & Checkout'} breadcrumb={breadcrumb}>
@@ -54,82 +85,127 @@ const Delivery = props => {
 
                             <Col xs={24}>
                                 <Item name={'address'} label={'Address'} className={'mb-0'}>
-                                    <Radio.Group className={'flex flex-col'}>
-                                        <Radio value={1} className={'py-2'}>Address Line 2, Address Line 1, City, State, Country, Zip Code</Radio>
-                                        <Radio value={2} className={'py-2'}>Address Line 2, Address Line 1, City, State, Country, Zip Code</Radio>
-                                        <Radio value={3} className={'py-2'}>Address Line 2, Address Line 1, City, State, Country, Zip Code</Radio>
-                                        <Radio value={4} className={'py-2'}>Address Line 2, Address Line 1, City, State, Country, Zip Code</Radio>
+                                    <Radio.Group className={'flex flex-col'} onChange={changeAddressHandler}>
+                                        {addresses.map(address => {
+
+                                            const transformed = address.addressLine2 ? [address.addressLine2] : [];
+                                            transformed.push(address.addressLine1);
+                                            transformed.push(address.city);
+                                            transformed.push(address.province);
+                                            transformed.push(address.country);
+
+                                            return (
+                                                <Radio value={address._id} className={'py-2'}>{transformed.join(", ")}</Radio>
+                                            )
+                                        })}
+                                        <div className="w-full">
+                                            <Divider className={'my-6'}/>
+                                        </div>
+                                        <Radio value={'new'} className={'py-2'}>Or add a new address</Radio>
                                     </Radio.Group>
                                 </Item>
                             </Col>
+                            {newAddress && (
+                                <>
+                                    <div className="w-full px-3">
+                                        <Divider className={'mt-6 mb-2'}/>
+                                    </div>
 
-                            <div className="w-full px-3">
-                                <Divider className={'my-8'}/>
-                            </div>
+                                    <Col span={24}>
+                                        <div className="mb-8 mt-6">
+                                            <GoogleMap
+                                                height={670}
+                                                initialCenter={{
+                                                    lat: 40.781305,
+                                                    lng: -73.9666857
+                                                }}
+                                                marker={marker}
+                                                clickHandler={changeMarkerPosition}
+                                            />
+                                        </div>
+                                    </Col>
 
-                            <Col xs={24}>
-                                <h3 className="text-type font-medium text-base mb-8">Or Add New Address</h3>
-                            </Col>
+                                    <Col lg={8} md={12} xs={24}>
+                                        <Item
+                                            name={'province'}
+                                            label={'Province'}
+                                            rules={[{
+                                                required: true,
+                                                message: 'Please select Province'
+                                            }]}
+                                        >
+                                            <Select
+                                                placeholder={'Select'}
+                                                onChange={setProvince}
+                                            >
+                                                {Object.keys(provinces).map(abbr => {
+                                                    return (
+                                                        <Option value={abbr}>{provinces[abbr]}</Option>
+                                                    )
+                                                })}
+                                            </Select>
+                                        </Item>
+                                    </Col>
+                                    <Col lg={8} md={12} xs={24}>
+                                        <Item name={'city'} label={'City'}
+                                              rules={[
+                                                  {
+                                                      required: true,
+                                                      message: 'Please Select City'
+                                                  }
+                                              ]}>
+                                            <Select
+                                                placeholder={province ? 'Select' : 'Select Province first'}
+                                            >
+                                                {cities.map(city => {
+                                                    return (
+                                                        <Option value={city[0]}>{city[0]}</Option>
+                                                    )
+                                                })}
+                                            </Select>
+                                        </Item>
+                                    </Col>
+                                    <Col span={24}>
+                                        <Item name={'address-line1'} label={'Address Line 1'}>
+                                            <Input.TextArea placeholder={'Address Line 1'} autoSize={{ minRows: 1, maxRows: 6 }} style={{ resize: 'none' }}/>
+                                        </Item>
+                                    </Col>
+                                    <Col span={24}>
+                                        <Item name={'address-line2'} label={'Address Line 2'}>
+                                            <Input.TextArea placeholder={'Address Line 2'} autoSize={{ minRows: 1, maxRows: 6 }} style={{ resize: 'none' }}/>
+                                        </Item>
+                                    </Col>
 
-                            <Col lg={8} md={12} xs={24}>
-                                <Item name={'province'} label={'Province'}>
-                                    <Select
-                                        placeholder={'Select'}
-                                    >
-                                        <Option value={'nyc'}>NYC</Option>
-                                        <Option value={'california'}>California</Option>
-                                        <Option value={'washington'}>Washington DC</Option>
-                                    </Select>
-                                </Item>
-                            </Col>
-                            <Col lg={8} md={12} xs={24}>
-                                <Item name={'city'} label={'City'}>
-                                    <Select
-                                        placeholder={'Select'}
-                                    >
-                                        <Option value={'nyc'}>NYC</Option>
-                                        <Option value={'california'}>California</Option>
-                                        <Option value={'washington'}>Washington DC</Option>
-                                    </Select>
-                                </Item>
-                            </Col>
-                            <Col span={24}>
-                                <Item name={'address-line1'} label={'Address Line 1'}>
-                                    <Input.TextArea placeholder={'Address Line 1'} autoSize={{ minRows: 1, maxRows: 6 }}/>
-                                </Item>
-                            </Col>
-                            <Col span={24}>
-                                <Item name={'address-line2'} label={'Address Line 2'}>
-                                    <Input.TextArea placeholder={'Address Line 2'} autoSize={{ minRows: 1, maxRows: 6 }}/>
-                                </Item>
-                            </Col>
-
-                            <Col lg={8} md={12} xs={24}>
-                                <Item name={'postal-code'} label={'Postal Code'}
-                                      rules={[
-                                          {
-                                              len: 5,
-                                              message: 'Postal Code Should be 5 characters',
-                                          },
-                                          {
-                                              required: true,
-                                              message: "Please enter Postal Code."
-                                          }
-                                      ]}>
-                                    <Input placeholder={'Postal Code'}/>
-                                </Item>
-                            </Col>
+                                    <Col lg={8} md={12} xs={24}>
+                                        <Item name={'postal-code'} label={'Postal Code'}
+                                              rules={[
+                                                  {
+                                                      len: 5,
+                                                      message: 'Postal Code Should be 5 characters',
+                                                  },
+                                                  {
+                                                      required: true,
+                                                      message: "Please enter Postal Code."
+                                                  }
+                                              ]}>
+                                            <Input placeholder={'Postal Code'}/>
+                                        </Item>
+                                    </Col>
+                                </>
+                            )}
 
                             <Col xs={24} className={'flex items-center flex-row-reverse pt-8'}>
                                 <Item>
-                                    <Button type="primary" className={'w-32 ml-5'}>
+                                    <Button type="primary" className={'w-32 ml-5'} htmlType={'submit'}>
                                         Next
                                     </Button>
                                 </Item>
                                 <Item>
-                                    <Button danger className={'w-32'}>
-                                        Prev
-                                    </Button>
+                                    <Link href={routes.cart.index}>
+                                        <Button danger className={'w-32'}>
+                                            Prev
+                                        </Button>
+                                    </Link>
                                 </Item>
                             </Col>
                         </Row>
@@ -143,6 +219,69 @@ const Delivery = props => {
 
 export async function getServerSideProps({ req, res }) {
 
+    let cookies = cookie.parse(req.headers.cookie || '');
+    let token = cookies.token
+    let userType = cookies.type;
+
+    let cart = {}
+    let addresses = [];
+    if (userType !== 'customer') {
+        res.writeHead(307, { Location: routes.auth.login });
+        res.end();
+        return {
+            props: {
+                authenticated,
+                cart,
+                addresses,
+            }
+        };
+    }
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    const store = getStore();
+
+    try {
+        const response = await store.dispatch.cart.getCart(config);
+        const addressesRes = await store.dispatch.profile.getAddresses(config)
+
+        if(response) {
+            cart = response;
+
+            if(cart.products.length === 0) {
+                res.writeHead(307, { Location: routes.cart.index });
+                res.end();
+                return {
+                    props: {
+                        cart,
+                        addresses,
+                    }
+                };
+            } else {
+
+                if(addressesRes) {
+                    addresses = addressesRes;
+                }
+                return {
+                    props: {
+                        cart,
+                        addresses,
+                    }
+                };
+            }
+        }
+    } catch(e) {
+        console.log(e);
+        return {
+            props: {
+                cart,
+                addresses,
+            }
+        }
+    }
 }
 
 export default Delivery;
