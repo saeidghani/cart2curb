2ea4dfaa540c8b9c640ba6bad4bbaa7ac9cab936
Hamid Ziadzadeh <hamidziadzadeh@gmail.com>
2020-11-04 20:29:44 +0330

    fix(product edit) :: fix error during save edits

--- pages/vendors/products/edit/[number].js
+++ pages/vendors/products/edit/[number].js
@@ -78,7 +78,7 @@ const EditProduct = props => {
             weight: weight || '',
             weightUnit: weightUnit || '',
             unitPrice: priceList.price || '',
-            category: category?.name || '',
+            category: category?._id || '',
             tax: tax || '',
             description: description || '',
             costPrice: priceList.cost || '',
