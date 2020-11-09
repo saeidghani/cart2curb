6f4630af1dc2e77746273b3513fb5a0d659d0b66
Hamid Ziadzadeh <hamidziadzadeh@gmail.com>
2020-11-09 18:38:27 +0330

    fix(custom server) :: fix port and base url

--- pages/api/cart/index.js
+++ pages/api/cart/index.js
@@ -13,7 +13,6 @@ export default async function handler(req, res) {
             res.status(200).json(response.data);
         } else {
             const response = await api.cart.cart();
-            console.log(response);
             res.status(200).json(response.data);
         }
     } catch(e) {
