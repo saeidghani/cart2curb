08a714f5461ee98df3fd5969751624a58086e669
saeid <saeidghani@gmail.com>
2021-01-26 10:38:53 +0330

    added video player and api

--- components/Admin/Profile/Video.js
+++ components/Admin/Profile/Video.js
@@ -1,9 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, Fragment} from 'react';
 import {Button, message, Upload, Form, Input, Row, Col} from 'antd';
 import {useRouter} from "next/router";
-import Link from "next/link";
-import ImgCrop from "antd-img-crop";
 import {useDispatch, useSelector} from "react-redux";
+import Link from 'next/link';
+
+import VideoPlayer from '../../UI/VideoPlayer';
 
 const {Item} = Form;
 
@@ -14,49 +15,54 @@ const Video = props => {
     const loading = useSelector(state => state.loading.effects.adminProfile.addCategory);
     const token = useSelector(state => state?.adminAuth?.token);
     const router = useRouter();
-    const [leftImageUrl, setLeftImageUrl] = useState('');
-    const [rightImageUrl, setRightImageUrl] = useState('');
+    const [leftVideoUrl, setLeftVideoUrl] = useState('');
+    const [rightVideoUrl, setRightVideoUrl] = useState('');
 
-    const submitHandler = async (values) => {
-        console.log(values);
-        const {name, parent, description} = values;
-        const body = {
-            name, parent, description
-        }
+    const submitHandler = async () => {
 
-        /*const res = await dispatch.adminProfile.addVideo({body, token});
-        if (res) {
+        const leftVideoBody = {
+            url: leftVideoUrl,
+            arrangementNum: 1
+        };
+        const rightVideoBody = {
+            url: leftVideoUrl,
+            arrangementNum: 1
+        };
+
+        const res1 = await dispatch.adminProfile.addVideo({body: leftVideoBody, token});
+        const res2 = await dispatch.adminProfile.addVideo({body: rightVideoBody, token});
+        if (res1 && res2) {
             router.push({pathname: routes.admin.profile.index, query: {tab: 'video'}});
-        }*/
+        }
     };
 
     function beforeUpload(file) {
         const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-        if (!isJpgOrPng) {
-            message.error('You can only upload JPG/PNG file!');
+        if (isJpgOrPng) {
+            message.error('You can not upload image!');
         }
-        const isLt2M = file.size / 1024 / 1024 < 2;
+        /*const isLt2M = file.size / 1024 / 1024 < 2;
         if (!isLt2M) {
             message.error('Image must smaller than 2MB!');
-        }
-        return isJpgOrPng && isLt2M;
+        }*/
+        return !isJpgOrPng;
     }
 
-    const handleLeftImgUpload = info => {
+    const handleLeftVideoUpload = info => {
         if (info.file.status === 'uploading') {
             return;
         }
         if (info.file.status === 'done') {
-            setLeftImageUrl(`${process.env.NEXT_PUBLIC_API_BASE_URL}v1/files/photos${info.file.response.data.path}`);
+            setLeftVideoUrl(`${process.env.NEXT_PUBLIC_API_BASE_URL}v1/files/photos${info.file.response.data.path}`);
         }
     };
 
-    const handleRightImgUpload = info => {
+    const handleRightVideoUpload = info => {
         if (info.file.status === 'uploading') {
             return;
         }
         if (info.file.status === 'done') {
-            setRightImageUrl(`${process.env.NEXT_PUBLIC_API_BASE_URL}v1/files/photos${info.file.response.data.path}`);
+            setRightVideoUrl(`${process.env.NEXT_PUBLIC_API_BASE_URL}v1/files/photos${info.file.response.data.path}`);
         }
     };
 
@@ -74,35 +80,34 @@ const Video = props => {
             strokeWidth: 2,
             format: percent => `${parseFloat(percent.toFixed(2))}%`,
         },
-    }
+    };
 
-    const UploadVideo = ({imageUrl, onUpload}) => (<ImgCrop>
-        <Upload
-            name="photo"
-            listType="picture-card"
-            className="avatar-uploader-wrapper border-0"
-            showUploadList={false}
-            beforeUpload={beforeUpload}
-            onChange={onUpload}
-            {...uploadProps}
-        >
-            <div className="">
-                {imageUrl && <img src={imageUrl} alt="avatar" style={{width: 580, height: 340}}/>}
-            </div>
-            {!imageUrl && <div
-                className={'full-rounded text-overline bg-card flex items-center justify-center'}
-                style={{width: 580, height: 340}}>
-            </div>}
-        </Upload>
-    </ImgCrop>);
+    const UploadVideo = ({videoUrl, onUpload}) => (<Upload
+        name="photo"
+        listType="picture-card"
+        className="avatar-uploader-wrapper border-0"
+        showUploadList={false}
+        beforeUpload={beforeUpload}
+        onChange={onUpload}
+        {...uploadProps}
+    >
+        <Fragment>
+            {videoUrl ? <VideoPlayer src={videoUrl}/> :
+                <div
+                    className={'full-rounded text-overline bg-card flex items-center justify-center'}
+                    style={{width: 580, height: 340}}
+                >
+                </div>}
+        </Fragment>
+    </Upload>);
 
     return (
         <div className="py-5 flex flex-col" style={{position: 'relative', top: 110, height: 460}}>
             <div className="flex flex-col">
                 <div className="mb-6 font-bold" style={{position: 'relative', top: '-125px'}}>Upload Your Videos</div>
                 <div className="flex space-x-2 mt-3">
-                    <UploadVideo imageUrl={leftImageUrl} onUpload={handleLeftImgUpload}/>
-                    <UploadVideo imageUrl={rightImageUrl} onUpload={handleRightImgUpload}/>
+                    <UploadVideo videoUrl={leftVideoUrl} onUpload={handleLeftVideoUpload}/>
+                    <UploadVideo videoUrl={rightVideoUrl} onUpload={handleRightVideoUpload}/>
                 </div>
             </div>
             <div className="flex justify-end space-x-2" style={{position: 'relative', top: 150}}>
@@ -111,8 +116,9 @@ const Video = props => {
                         type="primary"
                         block
                         className='w-full md:w-32 ml-0 md:ml-5'
-                        htmlType={'submit'}
+                        htmlType='submit'
                         loading={loading}
+                        onClick={submitHandler}
                     >
                         Save
                     </Button>
